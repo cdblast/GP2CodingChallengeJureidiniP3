b041c2de761dd2f7a2652a463561bce2285d6af5
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-10-21 10:55:56 -0700

    Challenge Upkeep
    Challenge Upkeep to choose which challenge to run
    Todo: make it to set the variable to true or false at the end to provide a true end to the program.

--- CodingChallengesJureidiniP3/Program.cs
+++ CodingChallengesJureidiniP3/Program.cs
@@ -1,103 +1,141 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Net.Security;
 
 class Challenges
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to my Coding Challenge program. There will be a variety of different functions you will have to choose from. Please pick one. (Provide a number. There are 7 challenges total currently.)");
-        /*
-        
-        var programChooser = Console.ReadLine();
-        int programChooserint = int.Parse(programChooserint);
+        var keepLoop = true;
 
-            if (programChooserint == 1) {
-                put all of challenge 1 in here :3
-                };
+        while (keepLoop)
+        {
+            Console.WriteLine("Welcome to my Coding Challenge program. There will be a variety of different functions you will have to choose from. Please pick one. (Provide a number. There are 7 challenges total currently.)");
 
-        for uhhhhh. failsafe?
-        this should be fine i think
-            else {
-                Console.WriteLine("That didn't work. Please provide a numerical value from 1-7 in order to jump to that challenge.");
+            var programChooser = Console.ReadLine();
+            int programChooserint = int.Parse(programChooser);
 
-                
-        */
+            if (programChooserint == 1)
+            {
+                // Challenge 1 - Increment of 1
+                Console.WriteLine("Today we are gonna use the number adder function called Sum. Please input two numbers for us to add.\n");
 
-        // Challenge 1 - Increment of 1
-        Console.WriteLine("Today we are gonna use the number adder function called Sum. Please input two numbers for us to add.\n");
+                var number1 = Console.ReadLine();
+                int number1int = int.Parse(number1);
 
-        var number1 = Console.ReadLine();
-        int number1int = int.Parse(number1);
+                Console.WriteLine("Great! Type one more number.");
 
-        Console.WriteLine("Great! Type one more number.");
+                var number2 = Console.ReadLine();
+                int number2int = int.Parse(number2);
 
-        var number2 = Console.ReadLine();
-        int number2int = int.Parse(number2);
+                Sum(number1int, number2int);
 
-        Sum(number1int, number2int);
+                Console.WriteLine("The sum of the number : " + number1 + " and the number: " + number2 + " is equal to: " + Sum(number1int, number2int));
+            }
+            if (programChooserint == 2)
+            {
+                //challenge 2 - minutes converter
+                Console.WriteLine("This program converts Minutes to Seconds. Please input Minutes.");
 
-        Console.WriteLine("The sum of the number : " + number1 + " and the number: " + number2 + " is equal to: " + Sum(number1int, number2int));
+                var minutesToConvert = Console.ReadLine();
+                int minutesToConvertint = int.Parse(minutesToConvert);
 
-        //challenge 2 - minutes converter
-        Console.WriteLine("This program converts Minutes to Seconds. Please input Minutes.");
+                Convert(minutesToConvertint);
 
-        var minutesToConvert = Console.ReadLine();
-        int minutesToConvertint = int.Parse(minutesToConvert);
+                Console.WriteLine("In " + minutesToConvert + " amount of minutes, there are " + Convert(minutesToConvertint) + " amount of seconds.");
 
-        Convert(minutesToConvertint);
 
-        Console.WriteLine("In " + minutesToConvert + " amount of minutes, there are " + Convert(minutesToConvertint) + " amount of seconnds.");
+            }
 
-        //challenge 3 - increment counter
-        Console.WriteLine("Provide a number.");
+            if (programChooserint == 3)
+            {
 
-        var challenge2Number = Console.ReadLine();
-        int challenge2Numberint = int.Parse(challenge2Number);
-         
-        Console.WriteLine("The given number " + challenge2Number + " with an increment of 1 is " + PlusOne (challenge2Numberint));
+                //challenge 3 - increment counter
+                Console.WriteLine("Provide a number.");
 
-        //Challenge 4 - what is the power of a voltage * current ?
-        Console.WriteLine("Provide a voltage.");
+                var challenge2Number = Console.ReadLine();
+                int challenge2Numberint = int.Parse(challenge2Number);
 
-        var Voltage = Console.ReadLine();
-        int Voltageint = int.Parse(Voltage);
+                Console.WriteLine("The given number " + challenge2Number + " with an increment of 1 is " + PlusOne(challenge2Numberint));
 
-        Console.WriteLine("Now provide a current.");
+            }
 
-        var Current = Console.ReadLine();
-        int Currentint = int.Parse(Current);
+            if (programChooserint == 4)
+            {
 
-        Console.WriteLine("The power will be " + Multiplier (Currentint, Voltageint));
+                //Challenge 4 - what is the power of a voltage * current ?
+                Console.WriteLine("Provide a voltage.");
 
-        //Challenge 5 - how many days old are you?
-        Console.WriteLine("Please provide an age to calculate.");
+                var Voltage = Console.ReadLine();
+                int Voltageint = int.Parse(Voltage);
 
-        var Age = Console.ReadLine();
-        int Ageint = int.Parse(Age);
+                Console.WriteLine("Now provide a current.");
 
-        Console.WriteLine("Someone " + Age + " years old is " + CalcAge(Ageint) + " days old.");
+                var Current = Console.ReadLine();
+                int Currentint = int.Parse(Current);
 
-        //Challenge 6 - calculate a triangle's area
+                Console.WriteLine("The power will be " + Multiplier(Currentint, Voltageint));
 
-        Console.WriteLine("Provide the base, then height to calculate the area of a triangle.");
+            }
 
-        var triBase = Console.ReadLine();
-        int triBaseint = int.Parse(triBase);
 
-        Console.WriteLine("Great! now provide the height.");
+            if (programChooserint == 5)
+            {
 
-        var triHeight = Console.ReadLine();
-        int triHeightint = int.Parse(triHeight);
+                //Challenge 5 - how many days old are you?
+                Console.WriteLine("Please provide an age to calculate.");
 
-        Console.WriteLine("The area of a triangle with a base of " + triBase + " and height of " + triHeight + " is " + triArea(triBaseint, triHeightint));
+                var Age = Console.ReadLine();
+                int Ageint = int.Parse(Age);
 
-        //Challenge 7 - Is a number equal to or less than 0?
+                Console.WriteLine("Someone " + Age + " years old is " + CalcAge(Ageint) + " days old.");
 
-        Console.WriteLine("Please provide a number to check is equal or less than 0.");
+            }
 
-        var lessThanZero = Console.ReadLine();
-        int lessThanZeroint = int.Parse(lessThanZero);
 
-        Console.WriteLine("The number provided is equal to or less than 0: " + lessThanOrEqualToZero(lessThanZeroint));
+            if (programChooserint == 6)
+            {
+
+                //Challenge 6 - calculate a triangle's area
+
+                Console.WriteLine("Provide the base, then height to calculate the area of a triangle.");
+
+                var triBase = Console.ReadLine();
+                int triBaseint = int.Parse(triBase);
+
+                Console.WriteLine("Great! now provide the height.");
+
+                var triHeight = Console.ReadLine();
+                int triHeightint = int.Parse(triHeight);
+
+                Console.WriteLine("The area of a triangle with a base of " + triBase + " and height of " + triHeight + " is " + triArea(triBaseint, triHeightint));
+
+            }
+
+
+            if (programChooserint == 7)
+            {
+
+                //Challenge 7 - Is a number equal to or less than 0?
+
+                Console.WriteLine("Please provide a number to check is equal or less than 0.");
+
+                var lessThanZero = Console.ReadLine();
+                int lessThanZeroint = int.Parse(lessThanZero);
+
+                Console.WriteLine("The number provided is equal to or less than 0: " + lessThanOrEqualToZero(lessThanZeroint));
+            }
+
+            //for uhhhhh. failsafe?
+            //this should be fine i think
+            else
+            {
+                Console.WriteLine("----\n This program will repeat now. If you are done, close the window. \n ----");
+            }
+
+
+        }
+
     }
 
     public static int Sum(int number1, int number2)
@@ -116,7 +154,7 @@ class Challenges
     {
         return number1 * number2;
     }
-    public static int CalcAge(int number) 
+    public static int CalcAge(int number)
     {
         return number * 365;
     }
